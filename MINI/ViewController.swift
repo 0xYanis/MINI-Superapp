7143c0ecb6f145fabb06b30376dc9225f382af26
Yanis <111576120+0xYanis@users.noreply.github.com>
2023-04-03 19:14:50 +0300

    Initial Commit

---
+++ MINI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MINI
+//
+//  Created by Yan Rybkin on 03.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
