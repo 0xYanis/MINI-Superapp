2f48f5a3f4e32e1645c5ef9448b646a5c43bac7a
Yanis <111576120+0xYanis@users.noreply.github.com>
2023-04-05 21:04:31 +0300

    Add Lottie Animation to login page

--- MINI/LoginPage/View/AnimationView.swift
+++ MINI/LoginPage/View/AnimationView.swift
@@ -7,15 +7,34 @@
 
 import UIKit
 import SnapKit
+import Lottie
 
 class AnimationView: UIView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .systemCyan
+        initalize()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private var animationView: LottieAnimationView?
+    
+}
+
+private extension AnimationView {
+    func initalize() {
+        backgroundColor = .systemCyan
+        animationView = .init(name: "scan")
+        animationView!.contentMode = .scaleAspectFit
+        addSubview(animationView!)
+        animationView?.snp.makeConstraints({ make in
+            make.center.equalToSuperview()
+            make.height.equalTo(220)
+        })
+        animationView?.loopMode = .autoReverse
+        animationView?.play()
     }
 }
