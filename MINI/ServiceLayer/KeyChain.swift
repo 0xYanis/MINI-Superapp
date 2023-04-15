f7a575022ccb44d455eb0d690c2c098bca8d12d0
Yanis <111576120+0xYanis@users.noreply.github.com>
2023-04-15 20:37:38 +0300

    Create KeyChainService

---
+++ MINI/ServiceLayer/KeyChain.swift
@@ -0,0 +1,58 @@
+//
+//  KeyChain.swift
+//  MINI
+//
+//  Created by Yan Rybkin on 15.04.2023.
+//
+
+import Foundation
+
+protocol KeyChainProtocol {
+    func getValue(forKey key: String) -> String?
+    func setValue(_ value: String, forKey key: String)
+    func removeValue(forKey key: String)
+}
+
+class KeyChain: KeyChainProtocol {
+    func getValue(forKey key: String) -> String? {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrAccount as String: key,
+            kSecReturnData as String: kCFBooleanTrue!,
+            kSecMatchLimit as String: kSecMatchLimitOne
+        ]
+        var dataTypeRef: AnyObject?
+        let status = withUnsafeMutablePointer(to: &dataTypeRef) {
+            SecItemCopyMatching(query as CFDictionary, UnsafeMutablePointer($0))
+        }
+        if status == errSecSuccess, let data = dataTypeRef as? Data {
+            return String(data: data, encoding: .utf8)
+        } else {
+            return nil
+        }
+    }
+    
+    func setValue(_ value: String, forKey key: String) {
+        let data = value.data(using: .utf8)!
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrAccount as String: key,
+            kSecValueData as String: data
+        ]
+        let status = SecItemAdd(query as CFDictionary, nil)
+        if status != errSecSuccess {
+            ///
+        }
+    }
+    
+    func removeValue(forKey key: String) {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrAccount as String: key
+        ]
+        let status = SecItemDelete(query as CFDictionary)
+        if status != errSecSuccess {
+            ///
+        }
+    }
+}
---
+++ MINI/ServiceLayer/KeyChainService.swift
@@ -0,0 +1,34 @@
+//
+//  KeyChainService.swift
+//  MINI
+//
+//  Created by Yan Rybkin on 15.04.2023.
+//
+
+import Foundation
+
+protocol KeyChainServiceProtocol {
+    func getValue(forKey key: String) -> String?
+    func setValue(_ value: String, forKey key: String)
+    func removeValue(forKey key: String)
+}
+
+class KeyChainService: KeyChainServiceProtocol {
+    private let keyChain: KeyChainProtocol
+    
+    init(keyChain: KeyChainProtocol = KeyChain()) {
+        self.keyChain = keyChain
+    }
+    
+    func getValue(forKey key: String) -> String? {
+        return keyChain.getValue(forKey: key)
+    }
+    
+    func setValue(_ value: String, forKey key: String) {
+        keyChain.setValue(value, forKey: key)
+    }
+    
+    func removeValue(forKey key: String) {
+        keyChain.removeValue(forKey: key)
+    }
+}
