6c013ecaeeb30c91346383b96296b67487c7f4e7
Yanis <111576120+0xYanis@users.noreply.github.com>
2023-04-16 12:13:30 +0300

    Create lottie animation for AnimationView UIView class

--- MINI/LoginModule/LoginAnimationView.swift
+++ MINI/LoginModule/LoginAnimationView.swift
@@ -7,14 +7,39 @@
 
 import UIKit
 import SnapKit
+import Lottie
 
 class LoginAnimationView: UIView {
     
+    private var animationView = LottieAnimationView(name: "hello")
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        initialize()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+private extension LoginAnimationView {
+    func initialize() {
+        configureAnimation()
+        createAnimationConstraints()
+        animationView.play()
+    }
+    func configureAnimation() {
+        animationView.frame = bounds
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = .loop
+        animationView.animationSpeed = 0.5
+    }
+    func createAnimationConstraints() {
+        addSubview(animationView)
+        animationView.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.height.equalToSuperview()
+        }
     }
 }
