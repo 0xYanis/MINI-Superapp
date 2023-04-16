59fdc25aebcce743293b7b9770cb2cf3937a0f24
Yanis <111576120+0xYanis@users.noreply.github.com>
2023-04-16 12:26:43 +0300

    Create elements of LoginView

--- MINI/LoginModule/LoginView.swift
+++ MINI/LoginModule/LoginView.swift
@@ -10,11 +10,25 @@ import SnapKit
 
 class LoginView: UIView {
     
+    private let helloLabel = UILabel(text: "Hello!", font: .boldSystemFont(ofSize: 20), color: .black)
+    private let nameLabel = UILabel(text: "Name", font: .systemFont(ofSize: 16), color: .black)
+    private let passLabel = UILabel(text: "Password", font: .systemFont(ofSize: 16), color: .black)
+    private let nameField = UITextField()
+    private let passField = UITextField()
+    private let loginButt = UIButton()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        initialize()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+private extension LoginView {
+    func initialize() {
+        
     }
 }
