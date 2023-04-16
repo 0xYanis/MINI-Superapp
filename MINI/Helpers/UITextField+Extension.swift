67601fd304349f8ab0baeb26a5979af611f37cd3
Yanis <111576120+0xYanis@users.noreply.github.com>
2023-04-16 13:00:28 +0300

    Create extension for UITextField

---
+++ MINI/Helpers/UITextField+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  UITextField+Extension.swift
+//  MINI
+//
+//  Created by Yan Rybkin on 16.04.2023.
+//
+
+import UIKit
+
+//extension UITextField {
+//    convenience init(label: String, lineColor: UIColor, cornerRadius: CGFloat = 10, height: CGFloat, width: CGFloat) {
+//
+//    }
+//}
