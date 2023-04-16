02c3034e1c8af97ca967030d8cc749f7cf9e9ad3
Yanis <111576120+0xYanis@users.noreply.github.com>
2023-04-16 13:10:40 +0300

    Add width argument for UITextField init extension

--- MINI/Helpers/UITextField+Extension.swift
+++ MINI/Helpers/UITextField+Extension.swift
@@ -6,13 +6,17 @@
 //
 
 import UIKit
+import SnapKit
 
 extension UITextField {
-    convenience init(label: String, borderColor: UIColor = .systemCyan, cornerRadius: Double = 10, fontSize: CGFloat = 18, height: CGFloat, width: CGFloat) {
+    convenience init(label: String, borderColor: UIColor = .systemCyan, cornerRadius: Double = 10, fontSize: CGFloat = 18, height: CGFloat = 60) {
         self.init(frame: .zero)
         self.roundCorners(radius: cornerRadius)
         self.backgroundColor = .white
         self.layer.borderColor = borderColor.cgColor
         self.font = .systemFont(ofSize: fontSize)
+        self.snp.makeConstraints { make in
+            make.height.equalTo(height)
+        }
     }
 }
