1f9bef6ed755cf819af444c963e1d921be69bd54
Yanis <111576120+0xYanis@users.noreply.github.com>
2023-04-27 18:23:30 +0300

    Configure template cell

---
+++ MINI/Bank/AllTemplatesModule/AllTemplatesViewCell.swift
@@ -0,0 +1,71 @@
+//
+//  AllTemplatesViewCell.swift
+//  MINI
+//
+//  Created by Yan Rybkin on 27.04.2023.
+//
+
+import UIKit
+
+final class AllTemplatesViewCell: UICollectionViewCell {
+    
+    static let cellId = "AllTemplatesViewCell"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initialize()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Private properties
+    private let templateView = UIView()
+    private let circleView = UIView()
+    private let imageView = UIImageView(image: UIImage(systemName: "gear"))
+    private let textLabel = UILabel(text: "Utility Payments", font: .systemFont(ofSize: 15), color: UIColor(named: "textColor"))
+}
+
+private extension AllTemplatesViewCell {
+    func initialize() {
+        backgroundColor = .clear
+        createTemplateView()
+        createCircleView()
+        createImageView()
+        createTextLabel()
+    }
+    func createTemplateView() {
+        templateView.backgroundColor = UIColor(named: "whiteColor")
+        templateView.radiusAndShadow(radius: 10, shadowSize: 7)
+        addSubview(templateView)
+        templateView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+    func createCircleView() {
+        circleView.backgroundColor = .systemGray2
+        templateView.addSubview(circleView)
+        circleView.roundCorners(radius: 20)
+        circleView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalToSuperview().inset(20)
+            make.width.height.equalTo(60)
+        }
+    }
+    func createImageView() {
+        imageView.tintColor = .black
+        circleView.addSubview(imageView)
+        imageView.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.width.height.equalToSuperview().inset(14)
+        }
+    }
+    func createTextLabel() {
+        templateView.addSubview(textLabel)
+        textLabel.snp.makeConstraints { make in
+            make.top.equalTo(circleView.snp.bottom).offset(15)
+            make.centerX.equalToSuperview()
+        }
+    }
+}
