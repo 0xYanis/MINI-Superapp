96925903fd407cbc8f106234aec01b05ef5812db
Yanis <111576120+0xYanis@users.noreply.github.com>
2023-04-28 10:04:24 +0300

    Use only dark style

--- MINI/Application/SceneDelegate.swift
+++ MINI/Application/SceneDelegate.swift
@@ -15,6 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
         let _ = AuthenticationManager.auth(window: window)
+        window.overrideUserInterfaceStyle = .dark 
         self.window = window
         window.makeKeyAndVisible()
     }
