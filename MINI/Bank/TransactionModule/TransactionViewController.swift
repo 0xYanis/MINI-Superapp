cefc3c722b5393b2a240d2f773c3f5e5da73db9f
Yanis <111576120+0xYanis@users.noreply.github.com>
2023-05-12 13:15:14 +0300

    Add table view

--- MINI/Bank/TransactionModule/TransactionViewController.swift
+++ MINI/Bank/TransactionModule/TransactionViewController.swift
@@ -13,7 +13,10 @@ protocol TransactionViewProtocol: AnyObject {
 }
 
 final class TransactionViewController: UIViewController {
+    
     var presenter: TransactionPresenterProtocol?
+    
+    private let tableView = UITableView(frame: .zero, style: .grouped)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,9 +37,36 @@ private extension TransactionViewController {
     func initialize() {
         view.backgroundColor = UIColor(named: "backColor")
         createNavigation()
+        createTableView(tableView: tableView)
     }
     
     func createNavigation() {
         navigationItem.title = "Transaction"
+    }
+    
+    func createTableView(tableView: UITableView) {
+        tableView.backgroundColor = .clear
+        tableView.separatorColor = .black
+        tableView.dataSource = self
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+}
+
+//MARK: - UITableViewDataSource
+extension TransactionViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "\(indexPath.row) cell"
+        cell.backgroundColor = .clear
+        cell.detailTextLabel?.text = "\(indexPath.row)"
+        cell.imageView?.image = UIImage(systemName: "note.text")
+        return cell
     }
 }
