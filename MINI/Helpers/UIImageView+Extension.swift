4d51d7bac75b658fdabb7f1a88ef8585001f8dd2
Yanis <111576120+0xYanis@users.noreply.github.com>
2023-05-24 20:13:52 +0300

    Small commit

--- MINI/Helpers/UIImageView+Extension.swift
+++ MINI/Helpers/UIImageView+Extension.swift
@@ -14,18 +14,4 @@ extension UIImageView {
         self.clipsToBounds = true
         self.contentMode = .scaleAspectFill
     }
-    
-    func loadImage(fromURL urlString: String?) {
-        guard let urlString = urlString else { return }
-        guard let url = URL(string: urlString) else { return }
-        URLSession.shared.dataTask(with: url) { [weak self] (data, _, _) in
-            if let data = data, let image = UIImage(data: data) {
-                DispatchQueue.main.async {
-                    self?.image = image
-                }
-            }
-        }.resume()
-    }
-    
-    
 }
