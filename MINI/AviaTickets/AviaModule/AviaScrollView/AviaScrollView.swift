219524a36e961337152c106c261e35cab0c1c3a0
Yanis <111576120+0xYanis@users.noreply.github.com>
2023-05-29 14:17:27 +0300

    Create scroll view

---
+++ MINI/AviaTickets/AviaModule/AviaScrollView/AviaScrollView.swift
@@ -0,0 +1,27 @@
+//
+//  AviaScrollView.swift
+//  MINI
+//
+//  Created by Yan Rybkin on 29.05.2023.
+//
+
+import UIKit
+
+final class AviaScrollView: UIScrollView {
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+private extension AviaScrollView {
+    func configure() {
+        
+    }
+}
