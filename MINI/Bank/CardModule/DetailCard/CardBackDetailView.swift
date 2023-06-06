b7122677e981b05d21b86722c8a0139f8c0738a7
Yanis <111576120+0xYanis@users.noreply.github.com>
2023-06-06 19:15:36 +0300

    Fix Card detail view

--- MINI/Bank/CardModule/DetailCard/CardBackDetailView.swift
+++ MINI/Bank/CardModule/DetailCard/CardBackDetailView.swift
@@ -10,13 +10,19 @@ import SkeletonView
 
 final class CardBackDetailView: UIView {
     
-    private lazy var cardView = UIView()
-    private lazy var lineView = UIView()
-    private lazy var cvvLabel = UILabel(
+    private lazy var cardView    = UIView()
+    private lazy var lineView    = UIView()
+    private lazy var cvvLineView = UIView()
+    private lazy var cvvLabel    = UILabel(
         text: "CVV: 123",
         font: .systemFont(ofSize: 18),
         numberOfLines: 1,
         color: .black
+    )
+    private lazy var bankLabel   = UILabel(
+        text: "BANK",
+        font: .boldSystemFont(ofSize: 28),
+        color: .silver
     )
     
     override init(frame: CGRect) {
@@ -31,14 +37,17 @@ final class CardBackDetailView: UIView {
     func configure(with data: BankCardEntity) {
         cardView.backgroundColor = UIColor(named: data.cardColor)
         cvvLabel.text = data.cvv
+        bankLabel.text = data.bankName
     }
 }
 
 private extension CardBackDetailView {
     func initialize() {
         createCardView()
-        createCvvLabel()
         createLineView()
+        createCvvlineView()
+        createCvvLabel()
+        createBankLabel()
     }
     
     func createCardView() {
@@ -52,23 +61,40 @@ private extension CardBackDetailView {
         }
     }
     
-    func createCvvLabel() {
-        
-        
-        addSubview(cvvLabel)
-        cvvLabel.snp.makeConstraints { make in
-            make.top.equalToSuperview().inset(35)
-            make.right.equalToSuperview().inset(50)
+    func createLineView() {
+        lineView.backgroundColor = .black
+        cardView.addSubview(lineView)
+        lineView.snp.makeConstraints { make in
+            make.left.right.equalToSuperview()
+            make.height.equalToSuperview().multipliedBy(0.2)
+            make.top.equalToSuperview().inset(20)
         }
     }
     
-    func createLineView() {
-        lineView.backgroundColor = .silver
-        addSubview(lineView)
-        lineView.snp.makeConstraints { make in
-            make.left.right.equalToSuperview().inset(20)
-            make.height.equalToSuperview().multipliedBy(0.2)
-            make.bottom.equalToSuperview().inset(40)
+    func createCvvlineView() {
+        cvvLineView.backgroundColor = UIColor(white: 0.2, alpha: 0.5)
+        cardView.addSubview(cvvLineView)
+        cvvLineView.snp.makeConstraints { make in
+            make.left.equalToSuperview()
+            make.right.equalToSuperview().multipliedBy(0.6)
+            make.height.equalToSuperview().multipliedBy(0.15)
+            make.centerY.equalToSuperview()
+        }
+    }
+    
+    func createCvvLabel() {
+        cvvLineView.addSubview(cvvLabel)
+        cvvLabel.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.right.equalToSuperview().inset(10)
+        }
+    }
+    
+    func createBankLabel() {
+        cardView.addSubview(bankLabel)
+        bankLabel.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.right.equalToSuperview().inset(20)
         }
     }
 }
