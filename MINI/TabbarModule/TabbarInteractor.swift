561969e824d5a122655f032e5c800df3a1f14d22
Yan Rybkin <ynchprod@gmail.com>
2023-06-08 10:07:51 +0300

    Delete MINI/TabbarModule directory

--- MINI/TabbarModule/TabbarInteractor.swift
+++
@@ -1,8 +0,0 @@
-//
-//  TabbarInteractor.swift
-//  MINI
-//
-//  Created by Yan Rybkin on 24.04.2023.
-//
-
-import Foundation
--- MINI/TabbarModule/TabbarPresenter.swift
+++
@@ -1,8 +0,0 @@
-//
-//  TabbarPresenter.swift
-//  MINI
-//
-//  Created by Yan Rybkin on 24.04.2023.
-//
-
-import Foundation
