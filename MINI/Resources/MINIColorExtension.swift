b01dc93b6ccb31dd103b60a575147291029824a1
Yanis <111576120+0xYanis@users.noreply.github.com>
2023-06-19 13:03:20 +0300

    Create color scheme

---
+++ MINI/Resources/MINIColorExtension.swift
@@ -0,0 +1,19 @@
+//
+//  MINIColorExtension.swift
+//  MINI
+//
+//  Created by Yan Rybkin on 19.06.2023.
+//
+
+import UIKit
+
+extension UIColor {
+    static var backMINI = UIColor(hex: "#FFFFFF") // white
+    static var back2MINI = UIColor(hex: "#F8F8F8") // dark white
+    
+    static var frontMINI = UIColor(hex: "#C2E3F6") // snow blue
+    static var front2MINI = UIColor(hex: "#191919") // black-blue
+    
+    static var tintMINI = UIColor(hex: "#FB7700") // orange
+    static var tint2MINI = UIColor(hex: "#2CAEE0") // light blue
+}
