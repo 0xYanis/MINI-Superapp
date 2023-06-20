17548009adbf6d9881c4bc862d5084a89b1a192a
Yanis <111576120+0xYanis@users.noreply.github.com>
2023-06-20 15:23:26 +0300

    Create Core Data service

---
+++ MINI/ServiceLayer/DataManager.swift
@@ -0,0 +1,8 @@
+//
+//  DataManager.swift
+//  MINI
+//
+//  Created by Yan Rybkin on 20.06.2023.
+//
+
+import Foundation
---
+++ MINI/ServiceLayer/DataService/DataService.swift
@@ -0,0 +1,17 @@
+//
+//  DataService.swift
+//  MINI
+//
+//  Created by Yan Rybkin on 20.06.2023.
+//
+
+import CoreData
+
+protocol DataServiceProtocol: AnyObject {
+    
+}
+
+final class DataService: DataServiceProtocol {
+    
+}
+
