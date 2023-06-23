85143b74d8756bff7de873e94e678dbb3cdecd86
Yanis <111576120+0xYanis@users.noreply.github.com>
2023-06-23 15:15:18 +0300

    Change from core data to realm

---
+++ MINI/ServiceLayer/RealmService.swift
@@ -0,0 +1,56 @@
+//
+//  RealmService.swift
+//  MINI
+//
+//  Created by Yan Rybkin on 23.06.2023.
+//
+
+import RealmSwift
+
+protocol RealmServiceProtocol {
+    func add<T: Object>(_ object: T)
+    func delete<T: Object>(_ object: T)
+    func update<T: Object>(_ object: T)
+    func fetch<T: Object>(_ objectType: T.Type) -> Results<T>?
+}
+
+final class RealmService: RealmServiceProtocol {
+    
+    private lazy var realm = try! Realm(configuration: .defaultConfiguration)
+
+    func add<T: Object>(_ object: T) {
+        do {
+            try realm.write {
+                realm.add(object, update: .all)
+            }
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func delete<T: Object>(_ object: T) {
+        do {
+            try realm.write {
+                realm.delete(object)
+            }
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func update<T: Object>(_ object: T) {
+        do {
+            try realm.write {
+                realm.add(object, update: .modified)
+            }
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func fetch<T: Object>(_ objectType: T.Type) -> Results<T>? {
+        let objects = realm.objects(objectType)
+        return objects.count > 0 ? objects : nil
+    }
+}
+
