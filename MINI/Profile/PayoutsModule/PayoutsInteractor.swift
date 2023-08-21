2d45dbafafe79cd5a2a631d4cdc21d9283646a99
Yanis <111576120+0xYanis@users.noreply.github.com>
2023-08-21 16:29:19 +0300

    Delete mock data

--- MINI/Profile/PayoutsModule/PayoutsInteractor.swift
+++ MINI/Profile/PayoutsModule/PayoutsInteractor.swift
@@ -15,13 +15,7 @@ final class PayoutsInteractor: PayoutsInteractorProtocol {
     
     weak var presenter: PayoutsPresenterProtocol?
     
-    private var payoutsData: [PayoutsModel] = [
-        .init(image: "AppIcon", title: "Чизбургер с беконом", description: "Сочный чизбургер с беконом для компании или одиноких посиделок перед грустным фильмом"),
-        .init(image: "AppIcon", title: "Чизбургер с беконом", description: "посиделок перед грустным фильмом"),
-        .init(image: "AppIcon", title: "Чизбургер с беконом", description: "Сочный чизбургер с беконом для компании или одиноких"),
-        .init(image: "AppIcon", title: "Чизбургер с беконом", description: "Сочный чизбургер "),
-        .init(image: "AppIcon", title: "Чизбургер с беконом", description: "Сочный чизбургер с беконом для компании или одиноких посиделок перед грустным фильмом Сочный чизбургер с беконом для компании или одиноких посиделок перед грустным фильмом")
-    ]
+    private var payoutsData: [PayoutsModel] = []
     
     func getData() -> [PayoutsModel] {
         return payoutsData
