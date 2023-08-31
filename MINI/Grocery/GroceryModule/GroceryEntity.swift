69bb6be6d306ffee7cc1c160daaf8698cb67def0
Yanis <111576120+0xYanis@users.noreply.github.com>
2023-09-01 00:14:47 +0300

    Create grocery type enum

--- MINI/Grocery/GroceryModule/GroceryEntity.swift
+++ MINI/Grocery/GroceryModule/GroceryEntity.swift
@@ -13,3 +13,25 @@ struct GroceryEntity: Codable, Equatable {
     let image: String
     let productName: String
 }
+
+enum GroceryType: String {
+    case fruit
+    case vegetable
+    case meat
+    case dairy
+    case grains
+    case snacks
+    case beverages
+    case seafood
+    case spices
+    case condiments
+    case drinks
+    case bread
+    case sweets
+    case frozenFood
+    case cannedFood
+    case pasta
+    case sauces
+    case babyFood
+    case petFood
+}
