4e27c1cbb0cda86d37f784937ab372236642ac7f
Yanis <111576120+0xYanis@users.noreply.github.com>
2023-09-04 16:16:33 +0300

    Add some methods for working with User's data

--- MINI/ServiceLayer/Firebase/FBFirestoreManager.swift
+++ MINI/ServiceLayer/Firebase/FBFirestoreManager.swift
@@ -5,14 +5,40 @@
 //  Created by Yan Rybkin on 31.08.2023.
 //
 
+import FirebaseAuth
 import FirebaseFirestore
 
 protocol FBFirestoreProtocol: AnyObject {
-    
+    func setUserData(user: User)
+    func updateUserData(uid: String?, updatedData: [String: Any])
 }
 
 final class FBFirestoreManager: FBFirestoreProtocol {
     
     private let db = Firestore.firestore()
     
+    public func setUserData(user: User) {
+        let path = db.collection("users").document(user.uid)
+        path.setData([
+            "name" : user.email?.getEmailName() ?? "User",
+            "email" : user.email ?? "",
+            "address" : "None"
+        ])
+    }
+    
+    public func updateUserData(uid: String?, updatedData: [String: Any]) {
+        guard let uid = uid else { return }
+        let path = db.collection("users").document(uid)
+        path.updateData(updatedData)
+    }
+    
+    
 }
+
+extension String {
+    func getEmailName() -> String? {
+        let components = self.components(separatedBy: "@")
+        return components.first
+    }
+}
+
