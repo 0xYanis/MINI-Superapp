91e2f724e51bf9a9ad44de5d0cdf57c949468ddb
Yanis Rybkin <ynchprod@gmail.com>
2023-11-30 17:58:29 +0300

    Added realm methods to transactions

--- MINI/Modules/BankFlow/TransactionModule/Transaction.swift
+++ MINI/Modules/BankFlow/TransactionModule/Transaction.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import RealmSwift
 
 struct Transaction: Identifiable, Codable {
     var id = UUID().uuidString
@@ -38,4 +39,59 @@ struct Transaction: Identifiable, Codable {
         ]
     }
     
+    static func convertToTransaction(_ object: TransactionObject) -> Transaction {
+        return Transaction(
+            id: object.id,
+            icon: object.icon,
+            name: object.name,
+            date: object.date,
+            cost: object.cost,
+            cardNumber: object.cardNumber,
+            location: object.location,
+            currency: object.currency,
+            status: object.status,
+            category: object.category,
+            notes: object.notes,
+            merchantID: object.merchantID,
+            customerID: object.customerID
+        )
+    }
+    
+}
+
+class TransactionObject: Object {
+    @Persisted var id = UUID().uuidString
+    @Persisted var icon: String?
+    @Persisted var name: String
+    @Persisted var date: String
+    @Persisted var cost: Double
+    @Persisted var cardNumber: Int
+    @Persisted var location: String?
+    @Persisted var currency: String
+    @Persisted var status: String
+    @Persisted var category: String?
+    @Persisted var notes: String
+    @Persisted var merchantID: Int
+    @Persisted var customerID: Int
+    
+    class func convertToObject(_ transaction: Transaction) -> TransactionObject {
+        let object = TransactionObject()
+        
+        object.id = transaction.id
+        object.icon = transaction.icon
+        object.name = transaction.name
+        object.date = transaction.date
+        object.cost = transaction.cost
+        object.cardNumber = transaction.cardNumber
+        object.location = transaction.location
+        object.currency = transaction.currency
+        object.status = transaction.status
+        object.category = transaction.category
+        object.notes = transaction.notes
+        object.merchantID = transaction.merchantID
+        object.customerID = transaction.customerID
+        
+        return object
+    }
+
 }
