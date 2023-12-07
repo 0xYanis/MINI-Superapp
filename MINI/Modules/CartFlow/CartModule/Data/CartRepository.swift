fd9c89df55e0bb07d8f3021f6b4b76beaef5152c
Yanis Rybkin <ynchprod@gmail.com>
2023-12-07 18:38:18 +0300

    Created methods for CartRepository

--- MINI/Modules/CartFlow/CartModule/Data/CartRepository.swift
+++ MINI/Modules/CartFlow/CartModule/Data/CartRepository.swift
@@ -8,6 +8,8 @@
 import Foundation
 
 protocol CartRepositoryProtocol: AnyObject {
+    var purchasesCount: Int { get }
+    
     func addPurchase(_ purchase: Purchase) throws
     func readPurchase(primaryKey: UUID) -> Purchase?
     func deletePurchase(key: UUID) throws
@@ -23,24 +25,33 @@ final class CartRepository: CartRepositoryProtocol {
         self.storage = RealmService()
     }
     
+    var purchasesCount: Int {
+        (try? storage.fetchAll(PurchaseObject.self).count) ?? 0
+    }
+    
     func addPurchase(_ purchase: Purchase) throws {
-        
+        let object = PurchaseObject(purchase)
+        try storage.add(object)
     }
     
     func readPurchase(primaryKey: UUID) -> Purchase? {
-        nil
+        guard let object = storage.read(PurchaseObject.self, key: primaryKey)
+        else { return nil }
+        return Purchase(object)
     }
     
     func deletePurchase(key: UUID) throws {
-        
+        try storage.delete(PurchaseObject.self, forKey: key)
     }
     
     func updatePurchase(_ purchase: Purchase) throws {
-        
+        let object = PurchaseObject(purchase)
+        try storage.update(object)
     }
     
     func fetchPurchases() throws -> [Purchase] {
-        []
+        let objects = try storage.fetchAll(PurchaseObject.self)
+        return objects.compactMap { Purchase($0) }
     }
     
     
